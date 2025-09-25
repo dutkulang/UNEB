a8635c951f6dd928ddaea1e69ac32010cc02453a
Dut Kulang <dutandrew78@gmail.com>
2025-09-25 12:17:17 +0200

    Create: model that handles user creation

---
+++ backend/src/models/User.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose"
+import validators from "validators";
+const { Schema, model } = mongoose;
+
+const userSchema = new Schema({
+    fullName: {
+        type:String,
+        required: true,
+        trim: true,
+        minLength: [5, "min length of 5 characters"],
+        maxLength: [50, "max length of 50 characters"]
+    },
+    email: {
+        type: String,
+        unqiue : true,
+        trim: true,
+        required: true,
+        lowercase: true,
+        validate(value){
+            if(!validators.isEmail(value)){
+                throw new Error("Invalid email address")
+            }
+        }
+    },
+    password: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    gender: {
+        type: String,
+        trim: true,
+        lowercase: true,
+        default: "not specified",
+        enum: ["male", "female", "not specified"]
+    },
+
+},
+{
+    timestamps:true
+}
+)
+
+export const User= model("User", userSchema)
