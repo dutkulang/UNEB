bb150f3547a38637b70a348eab8bd46182285e7f
Dut Kulang <dutandrew78@gmail.com>
2025-09-25 17:56:52 +0200

    Add: School model
    Handles School creation and its fields declaration

---
+++ backend/src/models/Schools.js
@@ -0,0 +1,43 @@
+import mongoose, {Schema, model} from "mongoose";
+
+
+// create / add districts
+
+
+const schoolSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+        minLength: [5, "School name must be greater than 5 characters"],
+        maxLength: [50, "School name cna be longer than 50 characters"]
+    },
+    centerNumber :{
+        type: Number,
+        required: true,
+        default: 1000
+    },
+    // location this can be state or district
+    district: {
+        type: String,
+
+    },
+    category:{
+        type: String,
+        enum: ['UPE', 'NUPE'],
+        default: "NUPE"
+    },
+    status:{
+        type: String,
+        enum: [
+            "active",
+            "in active",
+            "banned"
+        ],
+        default: "in active" 
+    }
+}, {
+    timestamps: true
+})
+
+export const School = model("School", schoolSchema);
