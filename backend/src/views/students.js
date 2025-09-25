d64a800b7f7bf377988d65cde068ec8118c491ef
Dut Kulang <dutandrew78@gmail.com>
2025-09-25 19:09:22 +0200

    Add: student view files

---
+++ backend/src/views/students.js
@@ -0,0 +1,21 @@
+import { isObjectIdOrHexString } from "mongoose";
+import { Student } from "../models/Student.js";
+
+export const createStudent = async(req, res) => {
+    const REQUIRED_STUDENT_FIELDS = ['fullName', 'dateOfBirth', "gender" ]
+    try{
+        const data = req.body;
+        const reqKeys  = Object.keys(data)
+        if(!REQUIRED_STUDENT_FIELDS.every(field => reqKeys.includes(field))){
+            throw new Error("missing required data fields, Student full name, date of Birth, gender")
+        }
+        const user  = await Student(data).save()
+        res.status(201).json({
+            user
+        })
+    } catch(error){
+        res.status(401).json({
+            "error": `ERROR: ${error.message}`
+        })
+    }
+}
