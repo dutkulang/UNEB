a91325e7e88f7db05160179e13fbf3fdd6e77248
Dut Kulang <dutandrew78@gmail.com>
2025-09-25 19:04:53 +0200

    Define route to Student

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,11 +3,13 @@ import dotenv from "dotenv";
 dotenv.config();
 import { connectDB } from "./config/db.js"
 import { schoolsRouter } from "./Router/schools.js";
+import { studentRouter } from "./Router/student.js";
 const app =  express()
 const PORT  = process.env.PORT || 3000
 
 app.use(express.json())
 app.use("/schools", schoolsRouter);
+app.use("/students", studentRouter)
 
 connectDB().then(
     ()=>{        
