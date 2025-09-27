72adee68171a172b4cffb21ec578c4cf6a55e1b0
Dut Kulang <dutandrew78@gmail.com>
2025-09-27 09:49:24 +0200

    got rid of old python (Django) code base

--- legacy/schools/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import School, SchoolType
-admin.site.register(School)
-admin.site.register(SchoolType)
--- legacy/subjects/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import Subject
-
-admin.site.register(Subject)
